fa9733a0186276315b407bbacadc825ef5273cd9
Michael <michael@michael-901.(none)>
2012-10-25 12:36:49 -0400

    * Looks like the last commit (a few min ago) for vpn.sh didn't quite work as needed (due to order, amping, or whatever). I fixed it, but now I can't verify this actually works because it appears all my testing has resulted in me being locked out by the system :\

--- helper/vpn.sh
+++ helper/vpn.sh
@@ -3,10 +3,12 @@
 # This script will open programs necessary to begin connecting to the VPN
 # service
 
-# disconnect from VPN, if necessary, and open new vpn connection
-/usr/bin/xterm -e "/usr/bin/sudo /usr/sbin/vpnc-disconnect; /usr/bin/sudo /usr/sbin/vpnc /home/michael/datalex/vpn/maltfield.config" &
-
 # open wikid
-/usr/bin/java -jar /home/michael/wikidtoken/wikidtoken.jar
+/usr/bin/java -jar /home/michael/wikidtoken/wikidtoken.jar &
 
-exit 0;
+# disconnect from VPN, if necessary, and open new vpn connection
+/usr/bin/xterm -e "/usr/bin/sudo /usr/sbin/vpnc-disconnect; /usr/bin/sudo /usr/sbin/vpnc /home/michael/datalex/vpn/maltfield.config; sleep 9"
+
+sleep 9999
+
+exit 0
